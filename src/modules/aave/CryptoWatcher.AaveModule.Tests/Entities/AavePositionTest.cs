e2dc4da30a380b687a67446aa1cea0d17d22a054
kobyakov <kobyakov@segmento.ru>
2025-09-08 23:51:23 +0300

    add more tests

--- src/modules/aave/CryptoWatcher.AaveModule.Tests/Entities/AavePositionTest.cs
+++ src/modules/aave/CryptoWatcher.AaveModule.Tests/Entities/AavePositionTest.cs
@@ -12,20 +12,25 @@ namespace CryptoWatcher.AaveModule.Tests.Entities;
 public class AavePositionTest
 {
     private static readonly Wallet TestWallet = new() { Address = Guid.CreateVersion7().ToString() };
+    private static readonly DateOnly TestDate = DateOnly.FromDateTime(DateTime.Now);
+    private readonly Fixture _fixture;
 
-    [Fact]
-    public void AddOrUpdateSnapshotTest_WhenPositionClosed_ShouldThrowException()
+    public AavePositionTest()
     {
-        var fixture = new Fixture();
-        fixture.Customize(new PositiveBigIntegerCustomization());
-        
-        var position = new AavePosition(AaveNetwork.CeloNetwork, TestWallet, AavePositionType.Borrowed,
-            fixture.Create<string>(), DateOnly.FromDateTime(DateTime.Now));
+        _fixture = new Fixture();
+        _fixture.Customize(new PositiveBigIntegerCustomization());
+    }
 
+    [Theory]
+    [InlineData(AavePositionType.Supplied)]
+    [InlineData(AavePositionType.Borrowed)]
+    public void AddOrUpdateSnapshotTest_WhenPositionClosed_ShouldThrowException(AavePositionType positionType)
+    {
+        var position = CreatePosition(positionType);
         position.ClosePosition(DateOnly.MaxValue);
-        
+
         Assert.Throws<InvalidOperationException>(() =>
-            position.AddOrUpdateSnapshot(fixture.Create<TokenInfo>(), 1, DateOnly.MaxValue));
+            position.AddOrUpdateSnapshot(_fixture.Create<TokenInfo>(), 1, TestDate));
     }
 
     [Theory]
@@ -33,61 +38,112 @@ public class AavePositionTest
     [InlineData(AavePositionType.Supplied)]
     public void AddOrUpdateSnapshotTest_WhenSnapshotForDayExist_ShouldUpdateSnapshot(AavePositionType type)
     {
-        var fixture = new Fixture();
-        
-        fixture.Customize(new PositiveBigIntegerCustomization());
+        var position = CreatePosition(type);
+        var token = _fixture.Create<TokenInfo>();
 
-        var syncDate = DateOnly.FromDateTime(DateTime.Now);
+        var existedTokenAmount = _fixture.Create<decimal>();
+        position.AddOrUpdateSnapshot(token, existedTokenAmount, TestDate);
 
-        var position = new AavePosition(AaveNetwork.CeloNetwork, TestWallet, type,
-            fixture.Create<string>(), DateOnly.FromDateTime(DateTime.Now));
+        var expectedTokenAmount = _fixture.Create<decimal>();
+        var expectedToken = token with
+        {
+            PriceInUsd = _fixture.Create<decimal>(),
+            Amount = _fixture.Create<decimal>()
+        };
 
-        var token = fixture.Create<TokenInfo>();
-
-        var existedSnapshot = new AavePositionSnapshot(position.Id, syncDate, token);
-        var existedTokenAmount = fixture.Create<decimal>();
-
-        var expectedSnapshot = new AavePositionSnapshot(position.Id, syncDate,
-            token with { PriceInUsd = fixture.Create<decimal>(), Amount = fixture.Create<decimal>() });
-
-        var expectedTokenAmount = fixture.Create<decimal>();
-        
-        position.AddOrUpdateSnapshot(existedSnapshot.Token, existedTokenAmount, syncDate);
-        
-        Assert.Equal(position.PreviousScaledAmount, existedTokenAmount);;
-        
-        position.AddOrUpdateSnapshot(expectedSnapshot.Token, expectedTokenAmount, syncDate);
+        position.AddOrUpdateSnapshot(expectedToken, expectedTokenAmount, TestDate);
 
         var actualSnapshot = position.PositionSnapshots.First();
 
         Assert.Single(position.PositionSnapshots);
         Assert.Equal(position.PreviousScaledAmount, expectedTokenAmount);
-        Assert.Equivalent(expectedSnapshot, actualSnapshot);
+        Assert.Equivalent(new AavePositionSnapshot(position.Id, TestDate, expectedToken), actualSnapshot);
     }
-    
+
     [Theory]
     [InlineData(AavePositionType.Borrowed)]
     [InlineData(AavePositionType.Supplied)]
     public void AddOrUpdateSnapshotTest_WhenSnapshotForDayNotExist_ShouldUpdateSnapshot(AavePositionType type)
     {
-        var fixture = new Fixture();
-        
-        fixture.Customize(new PositiveBigIntegerCustomization());
-
         var syncDate = DateOnly.FromDateTime(DateTime.Now);
+        var position = CreatePosition(type);
+        var token = _fixture.Create<TokenInfo>();
 
-        var position = new AavePosition(AaveNetwork.CeloNetwork, TestWallet, type,
-            fixture.Create<string>(), DateOnly.FromDateTime(DateTime.Now));
-
-        var token = fixture.Create<TokenInfo>();
-
-        var expectedSnapshot = new AavePositionSnapshot(position.Id, syncDate, token);
-
-        position.AddOrUpdateSnapshot(expectedSnapshot.Token, 1, syncDate);
+        position.AddOrUpdateSnapshot(token, 1, syncDate);
 
         var actualSnapshot = position.PositionSnapshots.First();
+        var expectedSnapshot = new AavePositionSnapshot(position.Id, syncDate, token);
 
         Assert.Single(position.PositionSnapshots);
         Assert.Equivalent(expectedSnapshot, actualSnapshot);
+    }
+
+    [Theory]
+    [InlineData(AavePositionType.Borrowed)]
+    [InlineData(AavePositionType.Supplied)]
+    public void AddOrUpdateSnapshotTest_WhenScaleNotChange_ShouldAddDepositEvent(AavePositionType type)
+    {
+        var syncDate = DateOnly.FromDateTime(DateTime.Now);
+        var expectedScaledAmount = _fixture.Create<decimal>();
+        var position = CreatePosition(type);
+        var token = _fixture.Create<TokenInfo>();
+
+        position.AddOrUpdateSnapshot(token, expectedScaledAmount, syncDate);
+        position.AddOrUpdateSnapshot(_fixture.Create<TokenInfo>(), expectedScaledAmount, syncDate.AddDays(1));
+
+        Assert.Single(position.PositionEvents);
+        AssertThatAaveEventCorrect(
+            position.PositionEvents.First(),
+            position.Id,
+            expectedScaledAmount,
+            AavePositionEventType.Deposit);
+    }
+
+    [Theory]
+    [InlineData(100, 150, AavePositionEventType.Deposit)]
+    [InlineData(100, 50, AavePositionEventType.Withdrawal)]
+    public void AddOrUpdateSnapshotTest_WhenScaleChange_ShouldUpdateSnapshot(
+        decimal oldScaleAmount,
+        decimal newScaleAmount,
+        AavePositionEventType eventType)
+    {
+        var position = CreatePosition(AavePositionType.Borrowed);
+        var token = _fixture.Create<TokenInfo>();
+
+        position.AddOrUpdateSnapshot(token, oldScaleAmount, TestDate);
+        position.AddOrUpdateSnapshot(token, newScaleAmount, TestDate);
+
+        var expectedAmount = eventType == AavePositionEventType.Deposit
+            ? newScaleAmount - oldScaleAmount
+            : oldScaleAmount - newScaleAmount;
+
+        Assert.Equal(2, position.PositionEvents.Count);
+        AssertThatAaveEventCorrect(
+            position.PositionEvents.Last(),
+            position.Id,
+            expectedAmount,
+            eventType);
+    }
+
+    private AavePosition CreatePosition(AavePositionType type)
+    {
+        return new AavePosition(
+            AaveNetwork.CeloNetwork,
+            TestWallet,
+            type,
+            _fixture.Create<string>(),
+            TestDate);
+    }
+
+    private static void AssertThatAaveEventCorrect(
+        AavePositionEvent @event,
+        Guid positionId,
+        decimal amount,
+        AavePositionEventType type)
+    {
+        Assert.Equal(positionId, @event.PositionId);
+        Assert.Equal(TestDate, @event.Date);
+        Assert.Equal(amount, @event.Amount);
+        Assert.Equal(type, @event.EventType);
     }
 }
