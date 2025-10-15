e029b487dada29fcfb6fbe9ab905f1a68bb59e33
kobyakov <kobyakov@segmento.ru>
2025-10-15 11:54:04 +0300

    add missing migrations

---
+++ src/CryptoWatcher.Infrastructure/Migrations/20251015085042_AddChainIdAndStateViewMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWatcher.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChainIdAndStateViewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SmartContractAddresses_NftManager",
+                table: "UniswapChainConfigurations");
+            
+            migrationBuilder.AddColumn<int>(
+                name: "ChainId",
+                table: "UniswapChainConfigurations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SmartContractAddresses_StateView",
+                table: "UniswapChainConfigurations",
+                type: "character(42)",
+                fixedLength: true,
+                maxLength: 42,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChainId",
+                table: "UniswapChainConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "SmartContractAddresses_StateView",
+                table: "UniswapChainConfigurations");
+        }
+    }
+}
